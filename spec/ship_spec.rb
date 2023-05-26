fe748d3da40004b79525b549536a5d7fdeabea8c
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-25 18:56:20 -0800

    test: add hit method test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -24,4 +24,19 @@ RSpec.describe Ship do
       expect(@cruiser.sunk?).to be false
     end
   end
+
+  describe '#hit' do
+    it 'can report hit' do
+      @cruiser.hit
+      expect(@cruiser.health).to eq 2
+      @cruiser.hit
+      expect(@cruiser.health).to eq 1
+
+      expect(@cruiser.sunk?).to be false
+
+      @cruiser.hit
+
+      expect(@cruiser.sunk?).to be true
+    end
+  end
 end
