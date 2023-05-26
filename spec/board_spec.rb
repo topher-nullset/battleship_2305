6fe5f317819b7dabd9570a1be91b1cfc2b26292f
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-26 15:18:17 -0800

    test: adds valid_placement? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Board do
   before do
     @board = Board.new
     @cruiser = Ship.new('cruiser', 3)
-    @submariner = Ship.new('submarine', 2)
+    @submarine = Ship.new('submarine', 2)
   end
 
   describe '#exists' do
@@ -25,6 +25,30 @@ RSpec.describe Board do
     expect(@board.valid_coordinate?('A5')).to be false
     expect(@board.valid_coordinate?('E1')).to be false
     expect(@board.valid_coordinate?('A22')).to be false
+    end
+  end
+
+  describe '#valid_placement?' do
+    it 'can place ships with correct number of coordinates' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
+    end
+
+    it 'can place ships on consecutive coordinates' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
+    end
+
+    xit 'cant place ships diagonaly' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
+    end
+
+    xit 'can place ships with valid coordinates' do
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
     end
   end
 end
