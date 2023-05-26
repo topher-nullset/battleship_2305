23fe6321430812bd7cca10719f58e9763e9cebdc
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-25 18:56:34 -0800

    feat: add hit method

--- lib/ship.rb
+++ lib/ship.rb
@@ -5,13 +5,21 @@ class Ship
     @name = name
     @length = length
     @sunk = false
+    @ship_health = length
   end
 
   def health
-    ship_health = length
+    @ship_health
   end
 
   def sunk?
+    if @ship_health == 0
+      @sunk = true
+    end
     @sunk
+  end
+
+  def hit
+    @ship_health -= 1
   end
 end
