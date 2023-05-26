59fcb2d0445e98ff54e09392d9560f6c1861e2ef
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-26 14:14:24 -0800

    test: add valid_coordinate? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Board do
       expect(@board.cells.length).to eq 16
     end
   end
+
+  describe '#validating_coordinates' do
+    it 'can validate coordinates' do
+    expect(@board.valid_coordinate?('A1')).to be true
+    expect(@board.valid_coordinate?('D4')).to be true
+    expect(@board.valid_coordinate?('A5')).to be false
+    expect(@board.valid_coordinate?('E1')).to be false
+    expect(@board.valid_coordinate?('A22')).to be false
+    end
+  end
 end
