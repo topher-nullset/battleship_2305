7f1e7a9b892ab54e0c9dab19f56999bda99b3493
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-28 22:21:18 -0800

    feat: started runner file with ideas for next steps

---
+++ runner.rb
@@ -0,0 +1,12 @@
+require './lib/cell'
+require './lib/ship'
+require './lib/board'
+
+# things we should think about before we start the runner file
+
+# 1. we need to create 1 more class, i was thinking a Game class
+  # Game class would include initiation and methods to interact with the player
+  # with logic that would repeat until the game was over
+
+# 2. double check that there is at least 1 test for each method.
+  # We should also add some integration tests.
