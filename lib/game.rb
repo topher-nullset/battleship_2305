3f1524347705f1d69af8f0b9d05bfab9a6ed2bfe
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-31 21:13:37 -0800

    fix: game logic update and minor gameplay tweaks

--- lib/game.rb
+++ lib/game.rb
@@ -13,13 +13,15 @@ class Game
   end
 
   def start
+    system("clear")
     display_welcome_message
     board_setup
     until game_over?
+      terminal_render
       user_turn
       cpu_turn
     end
-    display_goodbye_message
+    display_gameover_message
     board_reset
     self.start
   end
@@ -27,32 +29,62 @@ class Game
 private
 
   def display_welcome_message
-    puts "Welcome to BATTLESHIP"
+    puts "Welcome to BATTLESHIP, prepare to sink into oblivion!"
+    sleep(0.5)
     puts "Enter 'p' to play. Enter 'q' to quit."
     user_input = gets.chomp.downcase
     exit unless user_input == 'p'
+    system("clear")
   end
 
-  def display_goodbye_message
-    p "Bye bye!"
+  def display_gameover_message
+    if @cpu_cruiser.sunk? && @cpu_submarine.sunk?
+      sleep(0.2)
+      puts "Impossible, how did you do this?"
+      sleep(0.3)
+      puts "HOW could you?"
+      sleep(0.4)
+      puts "I have a fam..."
+    else
+      puts "Everything as it should be."
+      sleep(0.5)
+      puts "All your ship are belong to us!"
+    end
+    sleep(1)
+    puts "Would you like to play again? (y/n)"
+    user_input = gets.chomp.downcase
+    exit unless user_input == 'y'
   end
 
   def board_setup
     cpu_ship_placement
     p "I have laid out my ships on the grid."
+    sleep(1)
     p "You now need to lay out your two ships and get ready to LOSE, sucker."
+    sleep(1)
     p "The Cruiser is three units long and the Submarine is two units long."
+    sleep(0.3)
+    puts "=======PLAYER BOARD======="
+    sleep(0.3)
     print @p1_board.render
+    sleep(1)
     p "Enter the squares for the cruiser THAT I WILL SINK! (3 spaces)."
+    sleep(0.5)
     p "Please input your choice in the following format: A1 A2 A3"
     user_cruiser_choice = gets.chomp.upcase.split
     until @p1_board.valid_placement?(@p1_cruiser, user_cruiser_choice)
       p "Those are invalid coordinates. Try again."
       user_cruiser_choice = gets.chomp.upcase.split
     end
+    system("clear")
     @p1_board.place(@p1_cruiser, user_cruiser_choice)
+    sleep(0.3)
+    puts "=======PLAYER BOARD======="
+    sleep(0.3)
     print @p1_board.render(true)
+    sleep(0.5)
     p "Enter the squares for the submarine THAT I WILL DESTROY! (2 spaces)."
+    sleep(0.5)
     p "Please input your choice in the following format, and be sure not to overlap your cruiser: B1 B2"
     
     user_submarine_choice = gets.chomp.upcase.split
@@ -65,21 +97,34 @@ private
     p "You have successfully placed your ships. PREPARE TO DIE!"
   end
 
-  def user_turn
-    puts "=============COMPUTER BOARD============="
+  def terminal_render
+    system("clear")
+    puts "======COMPUTER BOARD======"
+    sleep(0.3)
     print @cpu_board.render
-    puts "==============PLAYER BOARD=============="
+    puts "=======PLAYER BOARD======="
+    sleep(0.3)
     print @p1_board.render(true)
+  end
+  
+  def user_turn
+    sleep(0.2)
     puts "Enter the coordinate for your shot with the letter followed by the number (i.e. D4):"
     user_input = gets.chomp
-
+    sleep(0.1)
     if @cpu_board.valid_coordinate?(user_input) && @cpu_board.cells[user_input].fired_upon? == false
       @cpu_board.cells[user_input].fire_upon 
       if @cpu_board.cells[user_input].empty?
         p1_hit_or_miss = "a miss."
       else
         p1_hit_or_miss = "a hit."
+        if @cpu_board.cells[user_input].ship.sunk?
+          p1_hit_or_miss += " You sunk my ship!"
+        end
       end
+      system("clear")
+      puts "SHOTS FIRED!!!!!"
+      sleep(0.5)
       puts "Your shot on #{user_input} was #{p1_hit_or_miss}"
       @p1_board.cells
     elsif @cpu_board.cells[user_input] == nil
@@ -93,8 +138,6 @@ private
   
   def game_over?
     (@cpu_cruiser.sunk? && @cpu_submarine.sunk?) || (@p1_cruiser.sunk? && @p1_submarine.sunk?)
-      # puts "Impossible, how did you do this? HOW could you? I have a fam..."
-      # puts "Everything as it should be. All your ship are belong to us!"
   end
 
   def cpu_turn
@@ -107,8 +150,13 @@ private
         cpu_hit_or_miss = "a miss."
       else
         cpu_hit_or_miss = "a hit."
+        if @p1_board.cells[cpu_choice].ship.sunk?
+          cpu_hit_or_miss += " I sunk your ship!"
+        end
       end
+      sleep(0.3)
     puts "My shot on #{cpu_choice} was #{cpu_hit_or_miss}"
+    sleep(2)
   end
     
   def cpu_ship_placement
